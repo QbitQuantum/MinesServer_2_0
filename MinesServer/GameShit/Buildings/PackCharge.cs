4a96d947c4c777d6e3bcbf33ef4adcf34f38ed69
Qbit <billchifer@bk.ru>
2026-06-12 14:24:30 +0700

    Добавляем класс PackCharge в пространство имен

--- MinesServer/GameShit/Buildings/PackCharge.cs
+++ MinesServer/GameShit/Buildings/PackCharge.cs
@@ -2,37 +2,40 @@
 using MinesServer.GameShit.Buildings;
 using MinesServer.GameShit.Enums;
 
-public abstract class PackCharge : PackDamage
+namespace MinesServer.GameShit.Buildings
 {
-    protected PackCharge() { }
-
-    protected PackCharge(int x, int y, int ownerid, int maxHp, int maxCharge) : base(x, y, ownerid, maxHp)
+    public abstract class PackCharge : PackDamage
     {
-        charge = maxCharge;
-        maxcharge = maxCharge;
-    }
+        protected PackCharge() { }
 
-    public virtual float charge { get; set; }
-    public virtual float maxcharge { get; set; }
-
-    // TODO: Стоит разделить эти методы на явные
-    public override void Damage(int i, DamageTypePacks DamageType = DamageTypePacks.Time)
-    {
-        if (ownerid == 0)
-            return;
-
-        switch (DamageType)
+        protected PackCharge(int x, int y, int ownerid, int maxHp, int maxCharge) : base(x, y, ownerid, maxHp)
         {
-            case DamageTypePacks.Raz:
-                charge = Math.Max(0, charge - 100);
-                // TODO: Наверное стоит убрать
-                // Потому что игровой цикл вызывает потоянный апдейт пака
-                if (charge == 0)
-                    Update();
-                break;
-            default:
-                base.Damage(i, DamageType);
-                break;
+            charge = maxCharge;
+            maxcharge = maxCharge;
+        }
+
+        public virtual float charge { get; set; }
+        public virtual float maxcharge { get; set; }
+
+        // TODO: Стоит разделить эти методы на явные
+        public override void Damage(int i, DamageTypePacks DamageType = DamageTypePacks.Time)
+        {
+            if (ownerid == 0)
+                return;
+
+            switch (DamageType)
+            {
+                case DamageTypePacks.Raz:
+                    charge = Math.Max(0, charge - 100);
+                    // TODO: Наверное стоит убрать
+                    // Потому что игровой цикл вызывает потоянный апдейт пака
+                    if (charge == 0)
+                        Update();
+                    break;
+                default:
+                    base.Damage(i, DamageType);
+                    break;
+            }
         }
     }
 }
